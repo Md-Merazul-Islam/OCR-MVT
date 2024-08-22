5ccd9aa4e649b1247c20eff68a2c4c1257e9e649
Md-Merazul-Islam <mdmerazul75@gmail.com>
2024-08-22 19:59:54 +0600

    update ok

--- myproject/urls.py
+++ myproject/urls.py
@@ -3,9 +3,12 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from . views import home
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home_page'),
     path('ocr/',include('ocr.urls')),
+    
 ]
 
 urlpatterns += static(settings.MEDIA_URL,
--- static/js/spur.js
+++ static/js/spur.js
@@ -1,13 +1,7 @@
-/*!
- * spur-template - An admin template based on Bootstrap 4
- * Version v1.1.0
- * Copyright 2016 - 2019 Alexander Rechsteiner
- * https://hackerthemes.com
- */
-
-const mobileBreakpoint = window.matchMedia("(max-width: 991px )");
+const mobileBreakpoint = window.matchMedia("(max-width: 991px)");
 
 $(document).ready(function(){
+    // Handle dropdown toggle
     $(".dash-nav-dropdown-toggle").click(function(){
         $(this).closest(".dash-nav-dropdown")
             .toggleClass("show")
@@ -19,19 +13,21 @@ $(document).ready(function(){
             .removeClass("show");
     });
 
+    // Handle menu toggle based on screen size
     $(".menu-toggle").click(function(){
         if (mobileBreakpoint.matches) {
             $(".dash-nav").toggleClass("mobile-show");
         } else {
-            $(".dash").toggleClass("dash-compact");
+            $(".dash-nav").toggleClass("dash-compact");
         }
     });
 
+    // Handle search box toggle
     $(".searchbox-toggle").click(function(){
         $(".searchbox").toggleClass("show");
     });
 
-    // Dev utilities
+    // Dev utilities (Optional: Automatically toggles the sidebar and search box for testing)
     // $("header.dash-toolbar .menu-toggle").click();
     // $(".searchbox-toggle").click();
 });
