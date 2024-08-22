e13d3e7c0d2c962a967fda6c0b5df17f50a70518
Md-Merazul-Islam <mdmerazul75@gmail.com>
2024-08-22 22:55:38 +0600

    update

--- myproject/urls.py
+++ myproject/urls.py
@@ -3,10 +3,11 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-from . views import home
+from . views import home,About
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home_page'),
+    path('about/', About, name='about_page'),
     path('ocr/',include('ocr.urls')),
     
 ]
